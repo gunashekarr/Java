2a2266650aae61f0774720b5e3835b7e3ce58af0
Gunashekar R <gunashekarr11@gmail.com>
2024-06-02 01:32:01 +0530

    Added Programs Folder

---
+++ src/programs/ReverseNumber.java
@@ -0,0 +1,41 @@
+/** @author Gunashekar R
+ *  gunashekarr11@gmail.com
+ */
+package programs;
+
+import java.util.Scanner;
+
+/**
+ * @author 91895
+ *
+ */
+public class ReverseNumber {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the Number:");
+		Scanner sc=new Scanner(System.in);
+		int num = sc.nextInt();
+		reverseUsingStringBuilder(num);
+		
+	}
+	public static void reverseUsingLoop(int num)
+	{
+		int rev=0;
+		// 1st Method
+		while(num!=0)
+		{
+			rev=num%10+num/10;
+		}
+		System.out.println("The Reveresed Number-->"+rev);
+		
+	}
+	public static void reverseUsingStringBuilder(int num)
+	{
+		StringBuilder sb=new StringBuilder();
+		StringBuilder result = sb.reverse();
+		System.out.println("The Reversed Number using String builder concept-->"+result);
+		
+	}
+
+}
