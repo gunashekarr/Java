0d1ab74e4e5f01e876fc61f82aa1a6fb559194c1
Gunashekar R <gunashekarr11@gmail.com>
2024-08-27 22:50:14 +0530

    Added Plaindrome program

---
+++ src/stringPrograms/Palindrome.java
@@ -0,0 +1,43 @@
+/** @author Gunashekar R
+ *  gunashekarr11@gmail.com
+ */
+package stringPrograms;
+
+/**
+ * @author 91895
+ *
+ */
+public class Palindrome {
+	public static void main(String[] args) {
+		String input = "level";
+		String sentance = "No lemon, no melon";
+		isPalindromeWord(input);
+		isPalindromeWords(sentance);
+	}
+	public static void isPalindromeWord(String input)
+	{
+		StringBuilder sc = new StringBuilder(input).reverse();
+		System.out.println("After StringBuilder : "+sc);
+		if(sc.toString().equals(input))
+		{
+			System.out.println("Its an Palindrome!!");
+		}
+		else
+		{
+			System.out.println("Its not an Palindrome!!");
+		}
+	}
+		public static void isPalindromeWords(String sentance)
+		{
+	        String[] words1=sentance.split(",");
+//			StringBuilder sc = new StringBuilder();
+			for(String words:words1)
+			{
+				StringBuilder sc1 = new StringBuilder(words);
+				System.out.print(sc1.reverse().toString()+" ");
+			}
+//			System.out.println("After StringBuilder : "+sc.reverse());
+				
+	}
+
+}
