b40be757c127db0b37548ebfcbf6be2ac0e041e4
Gunashekar R <gunashekarr11@gmail.com>
2024-08-29 21:29:43 +0530

    Added general programs

---
+++ src/stringPrograms/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+/** @author Gunashekar R
+ *  gunashekarr11@gmail.com
+ */
+package stringPrograms;
+
+/**
+ * @author 91895
+ *
+ */
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+		String input="Gunashekar";
+		char ch[]=input.toCharArray();
+		
+		for (int i = 0; i < ch.length; i++) {
+			for (int j = i+1; j < ch.length; j++) {
+				if(ch[i]==ch[j])
+				{
+					System.out.print(ch[i]+" ");
+				}
+			}
+		}
+	}
+
+}
---
+++ src/stringPrograms/WordsCount.java
@@ -0,0 +1,17 @@
+/** @author Gunashekar R
+ *  gunashekarr11@gmail.com
+ */
+package stringPrograms;
+
+/**
+ * @author 91895
+ * Count the Number of Words in a String
+ */
+public class WordsCount {
+public static void main(String[] args) {
+	String input = "I am the star of my world";
+	String words[] =input.split(" ");
+	System.out.println("The number of words in a string : "+words.length);
+	
+}
+}
