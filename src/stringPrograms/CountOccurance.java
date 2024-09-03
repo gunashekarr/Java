767b4d153d5fed419b2a81906aa096272fdae748
Gunashekar R <gunashekarr11@gmail.com>
2024-09-03 21:09:29 +0530

    added occurance program

---
+++ src/stringPrograms/CountOccurance.java
@@ -0,0 +1,26 @@
+/** @author Gunashekar R
+ *  gunashekarr11@gmail.com
+ */
+package stringPrograms;
+
+/**
+ * @author 91895
+ *
+ */
+public class CountOccurance {
+	public static void main(String[] args) {
+		String input = "test java";
+		char ch = ' ';
+		int count=0;
+		for(char chr:input.toCharArray())
+		{
+			if(chr==ch)
+			{
+				count++;
+			}
+		}
+		System.out.println("the number of occurance is : "+count);
+
+	}
+
+}
---
+++ src/stringPrograms/CountOccurances.java
@@ -0,0 +1,37 @@
+/** @author Gunashekar R
+ *  gunashekarr11@gmail.com
+ */
+package stringPrograms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 91895
+ *
+ */
+public class CountOccurances {
+	public static void main(String[] args) {
+		String input = "test";
+		countOccurances(input);
+	}
+	public static void countOccurances(String input)
+	{
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		for(char ch:input.toCharArray())
+		{
+			if(map.containsKey(ch))
+			{
+				map.put(ch, map.get(ch)+1);
+			}
+			else
+			{
+				map.put(ch, 1);
+			}
+		}
+		System.out.println("Name" + ":" + map);
+		
+	}
+
+}
